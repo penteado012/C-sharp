292a9dac25c7a8f3b43160f84c02f11bd254092c
Matheus Penteado <161362483+penteado012@users.noreply.github.com>
2025-10-27 23:12:22 -0300

    Iniciando projeto filas atendimento
    Feito a classe senha e classe senhas em desenvolvimento

---
+++ Projeto Filas Atendimento/Projeto Filas Atendimento/Senha.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Filas_Atendimento
+{
+    internal class Senha
+    {
+        private int id;
+        private DateTime dataGerac;
+        private DateTime dataAtend;
+
+        public Senha(int id)
+        {
+            this.Id = id;
+            this.dataGerac = DateTime.Now;
+            this.DataAtend = DateTime.Now;
+        }
+
+        
+        public DateTime DataGerac { get => dataGerac; set => dataGerac = value; }
+        public int Id { get => id; set => id = value; }
+        public DateTime DataAtend { get => dataAtend; set => dataAtend = value; }
+
+        public string dadosParciais()
+        {
+            return Id + "-" + DataGerac;
+        }
+
+        public string dadosCompletos()
+        {
+            return Id + "-" + DataGerac + "-" + DataAtend;
+        }
+}
+}
---
+++ Projeto Filas Atendimento/Projeto Filas Atendimento/Senhas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Filas_Atendimento
+{
+    internal class Senhas
+    {
+        private int proximoAtendimento;
+        private Queue<Senha> filaSenhas;
+
+        public void gerar()
+        {
+
+        }
+    }
+}
